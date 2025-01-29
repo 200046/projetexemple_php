4ff491930b9f45dd1db53deb7c93382ae547047b
boubby <boubby.dsc@gmail.com>
2025-01-29 13:06:01 +0100

    29-01-2025

---
+++ MARCEL_ETAPE2/Controllers/userController.php
@@ -0,0 +1,19 @@
+<?php // Code php qui décide de ce qu'il faut donner comme valeur à la variable $template
+ // On ajoutera par la suite les require auc modèles 
+
+ // Récupération du chemin désiré
+ $uri = $_SERVER["REQUEST_URI"];
+ if ($uri === "/connexion") {
+    $title = "Connexion";
+    $template = "Views/Users/connexion.php";
+    require_once("Views/base.php");
+ }
+ elseif ( $uri === "/deconnexion") {
+    // À voir plus tard
+ }
+ elseif ($uri === "/inscription") {
+    $title = "Inscription";
+    $template = "Views/Users/InscriptionOrEditProfile.php";
+    require_once("Views/base.php");
+ }
+?>
