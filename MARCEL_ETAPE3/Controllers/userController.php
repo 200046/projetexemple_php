0f58cac918432415608e260aab7dac5403e41ffb
boubby <boubby.dsc@gmail.com>
2025-01-29 16:05:50 +0100

    29-01-2025

--- MARCEL_ETAPE3/Controllers/userController.php
+++ MARCEL_ETAPE3/Controllers/userController.php
@@ -1,37 +1,37 @@
 <?php // Code php qui décide de ce qu'il faut donner comme valeur à la variable $template
- // On ajoutera par la suite les require auc modèles 
+// On ajoutera par la suite les require auc modèles 
 
- // Récupération du chemin désiré
- $uri = $_SERVER["REQUEST_URI"];
- if ($uri === "/connexion") {
-    // Vérifier si l'utilisateur a cliqué sur le bouton du form
-   if(isset($_POST['btnEnvoi'])) {
+// Récupération du chemin désiré
+$uri = $_SERVER["REQUEST_URI"];
+if ($uri === "/connexion") {
+   // Vérifier si l'utilisateur a cliqué sur le bouton du form
+   if (isset($_POST['btnEnvoi'])) {
       // Pour récupere l'erreur si l'utilisateur fait une faute de frappe
-      $erreur=false;
+      $erreur = false;
       // Tentative de connexion et de récuperation des données de l'utilisateur
-      if(connectUser($pdo)) {
+      if (connectUser($pdo)) {
          // Rédirection vers la page d'accueil
          header("location:/");
       }
    }
-    $title = "Connexion";
-    $template = "Views/Users/connexion.php";
-    require_once("Views/base.php");
- }
- elseif ( $uri === "/deconnexion") {
-    // À voir plus tard
- }
- elseif ($uri === "/inscription") {
-   if(isset($_POST['btnEnvoi'])) {
-      // Ajout de l'utilisateur à la base de données
-      createUser($pdo);
-      // Rédirection vers la page de connexion
-      header("location:/connexion");
-   } else { 
-      $erreur=true;
+   $title = "Connexion";
+   $template = "Views/Users/connexion.php";
+   require_once("Views/base.php");
+} elseif ($uri === "/deconnexion") {
+   // À voir plus tard
+} elseif ($uri === "/inscription") {
+   if (isset($_POST['btnEnvoi'])) {
+      // Vérif des données encodées
+      $messageError = verifEmptyData();
+      // S'il n'y a pas d'erreur
+      if (!$messageError) {
+         // Ajout de l'utilisateur à la base de données
+         createUser($pdo);
+         // Rédirection vers la page de connexion
+         header("location:/connexion");
+      }
    }
-    $title = "Inscription";
-    $template = "Views/Users/InscriptionOrEditProfile.php";
-    require_once("Views/base.php");
- }
-?>
+   $title = "Inscription";
+   $template = "Views/Users/InscriptionOrEditProfile.php";
+   require_once("Views/base.php");
+}
--- MARCEL_ETAPE3/Models/userModel.php
+++ MARCEL_ETAPE3/Models/userModel.php
@@ -23,4 +23,43 @@ function connectUser($pdo) {
         $message = $e->getMessage();
         die($message);
 }}
+
+function createUser($pdo) {
+    try {
+        // Définition de la requête d'insertion
+        $query = 'insert into utilisateur{nomUser, prenomUser, loginUser, passWordUser emailUser, role}
+        values (:nomUser, :prenomUser, :loginUser, :passWordUser, :emailUser, :role)';
+        // Préparation de la requête
+        $ajouterUser = $pdo->prepare($query);
+        // Execution en attribuant les valeurs récupérées
+        $ajouterUser->execute([
+            'nomUser' => $_POST["nom"],
+            'prenomUser' => $_POST["prenom"],
+            'loginUser' => $_POST["login"],
+            'passWordUser' => $_POST["mot_de_passe"],
+            'emailUser' => $_POST["email"],
+            'role' => 'user'
+        ]);
+    } 
+    catch (PDOException $e) {
+        $message = $e->getMessage();
+        die($message);
+    }
+}
+
+function verifEmptyData() {
+    // Parcours du tableau $_POST
+    foreach($_POST as $key => $value) {
+        // str-remplace remplace une chaine par une autre chaine donnée
+        if (empty(str_replace(' ', '', $value))) {
+            // On rempli un tableau associatif
+            $messageError[$key] = "Votre " . $key . " est vide";
+        }
+    }
+    if (isset($messageError)) {
+        return $messageError;
+    } else {
+        return false;
+    }
+}
 ?>
